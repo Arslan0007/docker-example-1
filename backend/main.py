8cdd5b94b078603de2fbeddf2aaf603408a4caed
arslan0007 <arsla.n7.cl@gmail.com>
2024-06-21 22:40:41 +0300

    read data from text file - backend and print it to frontend webpage

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, Request, Depends, HTTPException
+from fastapi import FastAPI, Request, Depends, HTTPException, UploadFile, File
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 import requests
 import datetime
+from fastapi.responses import PlainTextResponse
+from io import StringIO
+import json
+
 
 app = FastAPI(
     title="MyApp",
@@ -28,9 +32,16 @@ app.add_middleware(
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 
+
 def write_to_text(price, dtime):
     with open('pricelist.txt', 'a') as p:
         p.write("\n" + f'price : {price}, date : {dtime}')
+
+def read_to_text(pricelist):
+    with open('pricelist.txt', 'r') as f:
+        content = f.read()
+    print("\n" + pricelist + content)
+    return content
 
 
 @app.get("/")
@@ -40,4 +51,7 @@ async def root():
     response_usd_rate = response_json["bpi"]["USD"]["rate"]
     write_to_text(response_usd_rate, datetime.datetime.now())
     return {"message": response_usd_rate}
-    
+
+@app.get("/pricelist/")
+async def pricelist():
+    return read_to_text('pricelist.txt')
